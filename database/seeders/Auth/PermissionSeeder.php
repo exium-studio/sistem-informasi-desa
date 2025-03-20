cc85a833b5bd00eea6064069bbc9fea3d2737f1c
distrostudiodev <distrostudiodev@gmail>
2025-03-21 05:15:00 +0700

    FIXED: Update permission seeders

--- database/seeders/Auth/PermissionSeeder.php
+++ database/seeders/Auth/PermissionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders\Auth;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 
 class PermissionSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class PermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $permissions = [
+            ['name' => 'dashboard.view', 'description' => 'View Dashboard', 'group' => 'dashboard'],
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate([
+                'name' => $permission['name'],
+                'guard_name' => 'web',
+            ], [
+                'description' => $permission['description'],
+                'group' => $permission['group'],
+            ]);
+        }
     }
 }
